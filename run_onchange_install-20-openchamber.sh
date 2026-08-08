2112ba6c9c331d21e170e920ff4d60314da82363
Jaka Hudoklin <jaka@x-truder.net>
2026-08-08 11:41:30 +0200

    Fix openchamber install

--- run_onchange_install-20-openchamber.sh
+++ run_onchange_install-20-openchamber.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH" # requires bun for installation
 curl -fsSL https://raw.githubusercontent.com/openchamber/openchamber/main/scripts/install.sh | bash
